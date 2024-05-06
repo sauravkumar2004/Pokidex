9697fc36560ff64c8fd6442f14109d7896dabb01
sauravkumar2004 <121476013+sauravkumar2004@users.noreply.github.com>
2024-05-06 21:55:52 +0530

    Update Pokedex.jsx

--- src/componentes/Pokedex/Pokedex.jsx
+++ src/componentes/Pokedex/Pokedex.jsx
@@ -6,7 +6,6 @@ import './Pokedex.css';
 import PokemonDetails from '../PokemonDetails/PokemonDetails';
 
 function Pokedex() {
-
     const [searchTerm, setSearchTerm] = useState('');
 
     return (
